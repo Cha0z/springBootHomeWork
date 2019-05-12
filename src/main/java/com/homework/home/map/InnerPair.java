f7003a9a6d1f5b0fdfc7efb378663aea85669a4f
Oleksandr Borodin <oleksandr_borodin@epam.com>
2019-05-12 23:20:45 +0300

    own map put and get changes

--- src/main/java/com/homework/home/map/InnerPair.java
+++ src/main/java/com/homework/home/map/InnerPair.java
@@ -2,14 +2,55 @@ package com.homework.home.map;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.Objects;
 
+@NoArgsConstructor
 @AllArgsConstructor
-@Data
-public class InnerPair {
 
-    private Object key;
-    private Object value;
+public class InnerPair<K, V> {
+    private K key;
+    private V value;
+    private List<InnerPair<K, V>> elements;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InnerPair<?, ?> innerPair = (InnerPair<?, ?>) o;
+        return key.equals(innerPair.key) &&
+                value.equals(innerPair.value) &&
+                Objects.equals(elements, innerPair.elements);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value, elements);
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public List<InnerPair<K, V>> getElements() {
+        return elements;
+    }
+
+    public void setElements(List<InnerPair<K, V>> elements) {
+        this.elements = elements;
+    }
 }
--- src/main/java/com/homework/home/map/OwnMap.java
+++ src/main/java/com/homework/home/map/OwnMap.java
@@ -1,9 +1,11 @@
 package com.homework.home.map;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
-public class OwnMap {
+public class OwnMap<V, K> {
 
     private List<InnerPair> elements;
 
@@ -16,17 +18,66 @@ public class OwnMap {
         return elements.size();
     }
 
-    public void put(Object key, Object value) {
-        if (elements.stream().anyMatch(elements -> elements.equals(key))) {
-            elements.add(new InnerPair(key, value));
+    public V put(K key, V value) {
+        int numberOfBucket = numberOfBucket(key);
+        V oldValue = null;
+        if (!Optional.ofNullable(elements.get(numberOfBucket)).isPresent()) {
+            InnerPair innerPair = getInnerPair(key, value);
+            elements.add(numberOfBucket, innerPair);
         } else {
-            elements.stream().filter(elements -> elements.equals(key)).findFirst().get().setValue(value);
+            if (key.equals(elements.get(numberOfBucket).getKey())) {
+                oldValue = (V) elements.get(numberOfBucket).getValue();
+                elements.get(numberOfBucket).setValue(value);
+
+            } else {
+                elements.get(numberOfBucket).setKey(null);
+                List<InnerPair> innerPairList;
+                if (elements.get(numberOfBucket).getElements() == null) {
+                    innerPairList = new ArrayList<>();
+                    innerPairList.add(elements.get(numberOfBucket));
+                    InnerPair innerPair = getInnerPair(key, value);
+                    innerPairList.add(innerPair);
+                    elements.get(numberOfBucket).setElements(innerPairList);
+                } else {
+                    InnerPair innerPair = getInnerPair(key, value);
+                    elements.get(numberOfBucket).getElements().add(innerPair);
+                }
+            }
         }
+        return oldValue;
     }
 
-    public Object get(Object key) {
-        return elements.stream().filter(element -> element.equals(key))
-                .findFirst().orElse(null);
+    private InnerPair getInnerPair(K key, V value) {
+        InnerPair innerPair = new InnerPair();
+        innerPair.setKey(key);
+        innerPair.setValue(value);
+        return innerPair;
+    }
+
+    private int numberOfBucket(K key) {
+        return key.hashCode() % elements.size();
+    }
+
+    public V get(K key) {
+        int numberOfBucket = numberOfBucket(key);
+        Optional<InnerPair> checkingValue = Optional.ofNullable(elements.get(numberOfBucket));
+        V value = null;
+        if (checkingValue.isPresent()) {
+            InnerPair<K, V> innerPair = checkingValue.get();
+            if (innerPair.getKey().equals(key)) {
+                return innerPair.getValue();
+            } else {
+                List<InnerPair<K, V>> innerPairList = innerPair.getElements();
+                for (InnerPair<K, V> current : innerPairList) {
+                    if (current.getKey().equals(key)) {
+                        return current.getValue();
+                    }
+                }
+            }
+
+
+        }
+        return value;
     }
 
 }
