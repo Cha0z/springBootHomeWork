5f78855df25a7785bbfef43b692348642c85c19d
Oleksandr Borodin <Oleksandr_Borodin@epam.com>
2019-07-30 17:05:24 +0300

    for sync with home pc

--- src/test/java/com/homework/home/map/OwnMapTest.java
+++ src/test/java/com/homework/home/map/OwnMapTest.java
@@ -1,39 +1,38 @@
 package com.homework.home.map;
 
-import static org.junit.Assert.assertEquals;
-
-import com.homework.home.map.OwnMap;
-import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
 public class OwnMapTest {
 
-  private OwnMap ownMap;
+    private OwnMap ownMap;
 
-  private Integer testKey = 1;
-  private String testValue = "test";
-
-
-  @Before
-  public void init() {
-    ownMap = new OwnMap();
-  }
-
-  @Test
-  public void addingTest() {
-    ownMap.put(testKey, testValue);
-    assertEquals(testValue, ownMap.get(testKey));
-  }
+    private Integer testKey = 1;
+    private String testValue = "test";
 
 
-
-  private void addElementsToMap(Integer index) {
-    Random random = new Random();
-    while (index != 0) {
-      ownMap.put(random.nextInt(20), testValue);
-      index--;
+    @Before
+    public void init() {
+        ownMap = new OwnMap();
     }
-  }
+
+    @Test
+    public void addingTest() {
+        ownMap.put(testKey, testValue);
+        assertEquals(testValue, ownMap.get(testKey));
+    }
+
+
+    private void addElementsToMap(Integer index) {
+        Random random = new Random();
+        while (index != 0) {
+            ownMap.put(random.nextInt(20), testValue);
+            index--;
+        }
+    }
 
 }
